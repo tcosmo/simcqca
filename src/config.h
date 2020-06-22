c52fb469b77f1bd54bb15afc6181f139e896fc15
Cosmo <tristan.sterin@gmail.com>
2020-06-22 14:02:48 +0100

    Testing clang format pre-commit hook

---
+++ src/config.h
@@ -0,0 +1,2 @@
+#define simcqca_VERSION_MAJOR 0
+#define simcqca_VERSION_MINOR 3
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "config.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int a = 9;
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << i;
+        int b = 9;
+    }
+}
